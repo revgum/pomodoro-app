4d044862e90e2ba086126ca5e3dbcb14f4252ad1
Josh Gum <revgum@gmail.com>
2018-05-06 10:42:33 -0700

    adds jest tests and examples

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component<Props, State> {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="app-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
       </div>
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+// Basic test full rendering
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+// Shallow rendering test
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+// Jest matcher test
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1 className="App-title">Welcome to React</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+  // or use jest-enzyme matchers
+  expect(wrapper).toContainReact(welcome);
+});
+
+it("renders the snapshot", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
